54fc5648bcef73b2b9269fadbaaba3c633c1007a
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-02-10 14:05:51 -0800

    ft_strjoin optimized

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -20,28 +20,16 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char		*s3;
-	size_t		i;
-	size_t		j;
+	char	*s3;
 
-	i = 0;
-	j = 0;
-	if (!s1 || !s2)
-		return (NULL);
-	s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!s3)
-		return (NULL);
-	while (s1[i])
+	s3 = NULL;
+	if (s1 && s2)
 	{
-		s3[i] = s1[i];
-		i += 1;
+		s3 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+		if (!s3)
+			return (NULL);
+		ft_strcpy(s3, s1);
+		ft_strcat(s3, s2);
 	}
-	while (s2[j])
-	{
-		s3[i] = s2[j];
-		i += 1;
-		j += 1;
-	}
-	s3[i] = '\0';
 	return (s3);
 }
