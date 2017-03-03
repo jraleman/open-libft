ed5ecafe433e48217702716cb971f3d34afb4cdb
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-03-02 18:28:58 -0800

    fixing typos

--- srcs/ft_isendian.c
+++ srcs/ft_isendian.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_little_endian.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaleman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/02 18:10:50 by jaleman           #+#    #+#             */
 /*   Updated: 2017/03/02 18:17:34 by jaleman          ###   ########.fr       */
