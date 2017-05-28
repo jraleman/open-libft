36260da2f20c5b2652038ce29ee185700dbf7e02
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-05-27 19:55:17 -0700

    Adding new function, angle to degrees...

---
+++ srcs/ft_angle_to_degrees.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_angle_to_degrees.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/27 19:48:05 by jaleman           #+#    #+#             */
+/*   Updated: 2017/05/27 19:48:07 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Converts an angle to a degree.
+** If the angle is over 360 or under 0, start from the beginning.
+*/
+
+int		ft_angle_to_degrees(int angle)
+{
+	return (FT_ABS(angle % 360));
+}
