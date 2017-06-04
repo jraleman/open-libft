694769f093c6f9724fee3a9e15c77c943d9f5c61
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-03 19:40:12 -0700

    Fixing strlcpy, still missing some edge cases... I'll take a look later on

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -22,6 +22,10 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
+	size_t	len;
+
+	len = ft_strlen(src);
 	ft_strncpy(dst, src, size);
-	return (ft_strlen(src));
+	dst[len] = '\0';
+	return (len);
 }
