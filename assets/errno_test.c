9a0402aba0f892c3d866a615741005fa1d1d5980
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-06 14:31:08 -0700

    Adding errno test file

---
+++ assets/errno_test.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errno_test.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/05 20:42:29 by jaleman           #+#    #+#             */
+/*   Updated: 2017/06/05 20:42:29 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_errno.h"
+#include "libft.h"
+
+int		main(void)
+{
+	printf("errno    : %d\n", ft_errno(FT_ENOENT));
+	printf("strerror : %s\n", ft_strerror(FT_ENOENT));
+	return (0);
+}
