7e2c7affe7ca1ec34c3dcacf303f9a23db098e19
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-17 01:00:55 -0700

    Adding btree_apply_prefix.c

---
+++ srcs/btree_apply_prefix.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_prefix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/14 00:28:01 by jaleman           #+#    #+#             */
+/*   Updated: 2017/06/14 00:28:02 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "btree.h"
+
+/*
+** Applies the function given as argument to the item of each node,
+** using prefix traversal to search the tree.
+*/
+
+void	btree_apply_prefix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root)
+		return ;
+	(*applyf)(root->item);
+	btree_apply_prefix(root->left, applyf);
+	btree_apply_prefix(root->right, applyf);
+}
