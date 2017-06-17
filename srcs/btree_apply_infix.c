c31a4f93c06593f5e2f5733b06f0900537fd71fe
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-17 01:01:17 -0700

    Adding btree_apply_INfix.c

---
+++ srcs/btree_apply_infix.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_infix.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/14 00:27:47 by jaleman           #+#    #+#             */
+/*   Updated: 2017/06/14 00:27:48 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "btree.h"
+
+/*
+** Applies the function given as argument to the item of each node,
+** using infix traversal to search the tree.
+*/
+
+void	btree_apply_infix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root)
+		return ;
+	btree_apply_infix(root->left, applyf);
+	(*applyf)(root->item);
+	btree_apply_infix(root->right, applyf);
+}
