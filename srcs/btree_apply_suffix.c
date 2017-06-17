2a5ea0bdc21506cdb47ec31a89696a16fbe4f1c5
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-17 01:01:39 -0700

    Adding btree_apply_sufix.c

---
+++ srcs/btree_apply_suffix.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_suffix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/14 00:27:54 by jaleman           #+#    #+#             */
+/*   Updated: 2017/06/14 00:27:55 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "btree.h"
+
+/*
+** Applies the function given as argument to the item of each node,
+** using suffix traversal to search the tree.
+*/
+
+void	btree_apply_suffix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root)
+		return ;
+	btree_apply_suffix(root->left, applyf);
+	btree_apply_suffix(root->right, applyf);
+	(*applyf)(root->item);
+}
