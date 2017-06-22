ba05932f88b57c3a1106a1f9bab989dfca6d8ed0
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-22 09:33:00 -0500

    Adding btree_level_count.c

---
+++ srcs/btree_level_count.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_level_count.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/26 04:40:17 by jaleman           #+#    #+#             */
+/*   Updated: 2016/11/26 04:40:18 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "btree.h"
+#include "libft.h"
+
+/*
+** Returns the size of the largest branch passed as argument.
+*/
+
+int		btree_level_count(t_btree *root)
+{
+	return (root ? 1 + FT_MAX(btree_level_count(root->left), \
+				btree_level_count(root->right)) : 0);
+}
