d673a8b110c0bc9299794ec40b53d392a902ae21
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-22 09:34:12 -0500

    Updating btree.h

--- includes/btree.h
+++ includes/btree.h
@@ -35,9 +35,12 @@ typedef struct		s_btree
 */
 
 t_btree				*btree_create_node(void *item);
+int					btree_level_count(t_btree *root);
 void				btree_add_node(t_btree **root, t_btree *node);
 void				btree_apply_infix(t_btree *root, void (*applyf)(void *));
 void				btree_apply_suffix(t_btree *root, void (*applyf)(void *));
 void				btree_apply_prefix(t_btree *root, void (*applyf)(void *));
+void				*btree_search_item(t_btree *root, void *data_ref, \
+																int (*cmpf)(void *, void *));
 
 #endif
