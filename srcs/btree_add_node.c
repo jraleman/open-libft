630e4df072d34c4d28f900311ac4635711d65506
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-22 09:30:49 -0500

    Adding btree_add_node.c

---
+++ srcs/btree_add_node.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_add_node.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/14 00:27:37 by jaleman           #+#    #+#             */
+/*   Updated: 2017/06/14 00:27:38 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "btree.h"
+
+/*
+** Add a new node to a binary tree.
+** Takes the root of the tree as paramenter.
+*/
+
+void	btree_add_node(t_btree **root, t_btree *node)
+{
+	t_btree		*tmp;
+
+	tmp = *root;
+	if (tmp == NULL)
+	{
+		tmp = node;
+		*root = tmp;
+	}
+	else if (node->item < tmp->item)
+		btree_add_node(&(tmp->left), node);
+	else if (node->item > tmp->item)
+		btree_add_node(&(tmp->right), node);
+}
