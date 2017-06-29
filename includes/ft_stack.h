2edd198d9f0cefe4ff1ab0c1618b0f88160030b6
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-06-28 20:05:06 -0700

    Adding ft_stack.h

---
+++ includes/ft_stack.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stack.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/27 18:30:40 by jaleman           #+#    #+#             */
+/*   Updated: 2017/06/27 18:30:41 by jaleman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STACK_H
+# define FT_STACK_H
+
+/*
+** Stack structure.
+*/
+
+typedef struct		s_stack
+{
+	double			value;
+	struct s_stack	*next;
+}					t_stack;
+
+/*
+** Functions by type stack.h
+*/
+
+void				ft_stck_show(t_stack *top);
+void				ft_stck_push(t_stack *top, double newdata);
+double				ft_stck_pop(t_stack *top);
+
+#endif
